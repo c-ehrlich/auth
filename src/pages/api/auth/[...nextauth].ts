bebe3ebb23e854048ee7512bb6951a039c5917a4
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-03 18:26:46 +0100

    actual initial commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,12 +6,12 @@ import { env } from "../../../env/server.mjs";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
+    // session({ session, user }) {
+    //   if (session.user) {
+    //     session.user.id = user.id;
+    //   }
+    //   return session;
+    // },
   },
   // Configure one or more authentication providers
   providers: [
